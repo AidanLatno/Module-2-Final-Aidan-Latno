a2ef06e582a688b275a0dd41b32169ff8da7b7af
Aidan Latno <aidlatno@outlook.com>
2023-09-15 16:43:41 -0700

    Added comments and Class functionality

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-
 window.addEventListener('load', function(){
     const canvas = this.document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     canvas.width = 146*3;
     canvas.height = 248*3;
 
+
+    /*
+        This class detects and logs input
+        Every Scene will have an instance of an InputHandler
+        The InputHandler will log any key presses and log them in their Scene's "keys" list and remove them on key up
+    */
     class InputHandler {
         constructor(scene){
             this.scene = scene;
             window.addEventListener('keydown', event => {
-                console.log(event.key);
-                if(( (event.key === 'w') ||
-                     (event.key === 'a') ||
-                     (event.key === 's') ||
-                     (event.key === 'd')
-                ) && this.scene.keys.indexOf(event.key) === -1){
+                if(this.scene.keys.indexOf(event.key) === -1){
                     this.scene.keys.push(event.key);
-                } else if (event.key === ' '){
-                    this.scene.player.shootTop();
                 }
-
             });
             window.addEventListener('keyup', event =>{
                 if(this.scene.keys.indexOf(event.key) > -1){
@@ -30,27 +27,14 @@ window.addEventListener('load', function(){
 
     }
 
-    class Projectile {
-        constructor(scene,x,y){
-            this.scene = scene;
-            this.x = x;
-            this.y = y;
-            this.width = 10;
-            this.height = 3;
-            this.speed = 3;
-            this.markedForDeletion = false;
-        }
-        update(){
-            this.x += this.speed;
-            if(this.x > this.scene.width * 0.8) this.markedForDeletion = true;
-        }
-        draw(context){
-            context.fillStyle = 'yellow';
-            context.fillRect(this.x,this.y,this.width,this.height);
-        }
-
-    }
-
+    /*
+        Base class for any object that exists within the scene(game)
+        Contains:
+            Methods to detect for collision with other Actors
+            Position, Height, Width, Speed
+            Tags list to create groups of Actors
+            Deletion flag that tells the scene to delete the Actor from itself when set to true
+    */
     class Actor{
         constructor(x,y,sprite){
             this.x = x;
@@ -62,23 +46,49 @@ window.addEventListener('load', function(){
             this.speedY = 0;
             this.maxSpeed = 2;
             this.markedForDeletion = false;
+            this.tags = [];
         }
+        // Draws the sprite with the info of the actor, gets called every frame
         draw(context){
-            console.log(this.x);
-            console.log(this.y);
             context.drawImage(this.sprite,this.x,this.y,this.width,this.height);
         }
+        // contains logic for Actor, should be overriden, gets called every frame
         update(){
             this.x += this.speedX;
             this.y += this.speedY;
         }
+        // Returns true if the actor passed in is colliding with this
+        isColliding(actor){
+            return !(
+                ((this.y + this.height) < (actor.y)) ||
+                (this.y > (actor.y + actor.height)) ||
+                ((this.x + this.width) < actor.x) ||
+                (this.x > (actor.x + actor.width))
+            );
+        }
+        // Adds tag to tag list for grouping
+        addTag(tag){
+            this.tags.push(tag);
+        }
+        // returns index of passed in tag within the tag list
+        // will return -1 if it does not exist with the list
+        hasTag(tag){
+            return this.tags.indexOf(tag);
+        }
     }
 
+    /*
+        Main Player class, inherits from Actor
+        Contains movement/input logic for the player
+    */
     class Player extends Actor{
         constructor(x,y,sprite){
             super(x,y,sprite);
             this.height = 84;
             this.width = 36;
+            this.score = 0;
+            this.lives = 3;
+            this.inventory = 0;
         }
         update(scene){
             this.speedX = 0;
@@ -98,16 +108,27 @@ window.addEventListener('load', function(){
         }
     }
 
+    /*
+        Trash class inherits from actor and is used for the trash object seen in game
+        Objects of this class will slowly fall down the scene, then delete themsekves once they get to the bottom 
+    */
     class Trash extends Actor{
         constructor(x,y,sprite){
             super(x,y,sprite);
         }
         update(scene){
             this.y += 1;
+            if(this.y > 700){
+                this.markedForDeletion = true;
+                console.log("Removed");
+            }
         }
     }
 
-
+    /*
+        Class containing all Actor in the world during runtime, aswell as trigging their logic and draw calls
+        Also contains input que and methods to remove/add Actors into the scene
+    */
     class Scene {
         constructor(width,height){
             this.width = width;
@@ -119,6 +140,10 @@ window.addEventListener('load', function(){
         update(){
             this.actors.forEach(actor =>{
                 actor.update(this);
+                if(actor.markedForDeletion === true){
+                    var index = this.actors.indexOf(actor);
+                    this.actors.splice(index,1);
+                }
             });
         }
         draw(context){
@@ -132,21 +157,36 @@ window.addEventListener('load', function(){
 
     }
     
+    // Declaration and initialization of constant game objects
     const level1 = new Scene(canvas.width,canvas.height);
     const player = new Player(100,100,document.getElementById("playertexture"));
-    console.log(player.sprite);
-    const randomTrash = new Trash(250,0,document.getElementById("trashtexture"));
 
     level1.addActor(player);
-    level1.addActor(randomTrash);
+
+    var intervalId = setInterval(function() {
+        console.log("Called");
+        level1.addActor(new Trash(200,20,document.getElementById("trashtexture")).addTag("Trash"));    
+    }, 5000);
 
 
-    // main animation loop
-    function animate(){
+    // main game loop
+    function game(){
+        // Graphics
         ctx.clearRect(0,0,canvas.width,canvas.height);
         level1.update();
         level1.draw(ctx);
-        requestAnimationFrame(animate);
+
+        // Detect Player collision with trash
+        level1.actors.forEach(actor =>{
+            if(actor.hasTag("Trash")){
+                if(actor.isColliding(player) && player.inventory < 10){
+                    actor.markedForDeletion = true;
+                    player.inventory++;
+                }
+            }
+        });
+
+        requestAnimationFrame(game);
     }
-    animate();
+    game();
 });
